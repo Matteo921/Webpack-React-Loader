dbd10a815f69b0e1c466002d5d776e89eb26a8be
Mateusz Cieślakowski <cieslakowski.mat@gmail.com>
2019-11-24 20:16:00 +0100

    correct code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
     constructor(props){
@@ -17,7 +18,7 @@ class App extends React.Component {
         const data = [...this.state.data, todo];
         this.setState({data});
     }
-}
+
 removeTodo(id) {
     const remainder = this.state.data.filter(todo => todo.id !== id);
     this.setState({data: remainder});
@@ -25,8 +26,10 @@ removeTodo(id) {
 render() {
     return (
         <div className={style.TodoApp}>
-            Tutaj pojawią się komponenty naszej aplikacji.
+           <Title title='To do list' length={this.state.data.length}/>
         </div>
     );
+ }
 }
 export default App;
+
